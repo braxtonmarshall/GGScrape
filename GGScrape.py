a392cad45faf3185df067a024b26239a2be8b835
unknown <marshallb121@gmail.com>
2020-09-04 17:51:34 -0400

    Improved crawler, utilizing threads rather than new processes

--- GGScrape.py
+++ GGScrape.py
@@ -1,150 +1,135 @@
 import requests
 import os
-import codecs
-import time
 from bs4 import BeautifulSoup
-from multiprocessing import Queue, Manager, Pool, cpu_count
+import concurrent.futures
+MAX_THREADS = 5
 
 class GGScraper (object):
     """ This class is a webscraper for the op.gg website that aims to collect summoner names
-    and summoners ranks for match data collection.
+    ranks, and total games played for match data collection.
     """
 
-    def __init__ (self, BaseURL, fileOut, region, baseRank):
+    def __init__(self, BaseURL, fileOut, region, baseRank):
         """ GGScraper Constructor"""
 
         self.BaseURL = BaseURL
-        self.fileOut = fileOut
+        self.file = fileOut
         self.region = region
-        self.baseRank = ''.join([i for i in baseRank if not i.isnumeric()]).rstrip()
-        self.rank_array = ["Challenger", "Grandmaster", "Master", "Diamond", "Platinum", "Gold", "Silver", "Bronze", "Iron"]
-
+        self.baseRank = ''.join(
+            [i for i in baseRank if not i.isnumeric()]).rstrip().lower()
+        self.rank_array = ["challenger", "grandmaster", "master",
+                           "diamond", "platinum", "gold", "silver", "bronze", "iron"]
         try:
             self.target_index = self.rank_array.index(self.baseRank) + 1
         except ValueError:
             self.target_index = 3
             print("Rank is not valid. Stopping after Master.")
+        self.bad_rank_int = 0
+        self.page_tracker = 1
+        self.findSummoners()
 
-        # Multiprocessing Manager for writer Queue
-        manager = Manager()
-        self.writerQueue = manager.Queue()
-        
-    def runCrawler(self):
-        """ Run GGCrawler asychronously"""
+    def process_futures(self, fs, exe):
+        """ Clear futures_done set to allow executor to continue grabbing html from urls """
+        for future in fs:
+            html = future.result()
+            if self.bad_rank_int > 600:
+                exe.shutdown(wait=False)
+                return
+            dict_to_write = self.parse_html(html, self.page_tracker)
+            if type(dict_to_write) is dict:
+                self.write_to_file(dict_to_write)
+                del dict_to_write
+                self.page_tracker += 1
+        fs.clear()
 
-        # Create Pools
-        pool = Pool(cpu_count() + 2)
-        jobs = []
-
-        # Start queue for writing to file
-        if self.fileOut != None:
-            writer = pool.apply_async(self.write_to_file)
-
-        # Create worker jobs
-        for i in range(1,10000):
-            jobs.append(pool.apply_async(self.findSummoners, ([i])))
-
-        # Wait for jobs to finish
-        for job in jobs:
-            time.sleep(.5)
-            result = job.get()
-            if result is True:
-                break
-
-        # Clean up, wait for queue to push end message
-        if self.fileOut != None:
-            self.writerQueue.put("kill")
-            writer.get()
-        pool.close()
-        pool.terminate()
-        pool.join()
-    
-    def findSummoners(self, count):
+    def findSummoners(self):
         """ Find all summoners on Op.gg """
 
-        temp_dict = {}
+        threads = MAX_THREADS
+        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as exe:
+            # enforcing an upper-bound limit on threads to keep memory usage low using sets of future sequences
+            futures_done = set()
+            futures_notdone = set()
+            urls = self.generate_urls()
+            for url in urls:
+                try:
+                    futures_notdone.add(exe.submit(self.download_html, url))
+                # sending exe.shutdown(wait=false) to exit early generates a run-time error
+                except RuntimeError:
+                    return
+                if len(futures_notdone) >= MAX_THREADS:
 
-        url = self.generate_urls(count)
-        currPage = self.download_html(url)
-        
-        print("[+] Parsing Page Number " + str(count) + "\tRegion: " + self.region)
-        html = BeautifulSoup(currPage.text, 'lxml')
-        temp_dict, bad_rank_count = self.parse_html(html)
-
-        # Send to writer queue
-        if type(temp_dict) is dict:
-            for key in temp_dict:
-                input = key + "/" + temp_dict[key][0] + "/" + temp_dict[key][1]
-                self.writerQueue.put(input)
-        # stop processing jobs
-        if type(bad_rank_count) is int and bad_rank_count > 10:
-            return True
-        return False
+                    done, futures_notdone = concurrent.futures.wait(
+                        futures_notdone, return_when=concurrent.futures.FIRST_COMPLETED)
+                    futures_done.update(done)
+                    self.process_futures(futures_done, exe)
 
     def download_html(self, url):
         """ Download HTML from a URL and return"""
 
         currPage = requests.get(url)
-        return currPage
+        html = BeautifulSoup(currPage.text, 'lxml')
+        return html
 
-    def generate_urls(self, count):
+    def generate_urls(self):
         """ Generate and return a list of URLs"""
 
-        url = self.BaseURL + "page={}".format(count)
-        return url
+        urls = list()
+        for i in range(1, 3000):
+            urls.append(self.BaseURL + "page={}".format(i))
+        return urls
 
-    def write_to_file(self):
+    def parse_html(self, html, pagenum):
+        """ Parse HTML for summoner name and rank and return a dictionary"""
+
+        print("[+] Parsing Page Number " +
+              str(pagenum) + "\tRegion: " + self.region)
+        summoner_dict = {}
+        results = html.find_all("tr", attrs={"class": "ranking-table__row"})
+        for result in results:
+            try:
+                SummonerName = result.find(
+                    "td", attrs={"class": "select_summoner ranking-table__cell ranking-table__cell--summoner"}).select_one("span").text
+                Current_Rank = result.find(
+                    "td", attrs={"class": "ranking-table__cell ranking-table__cell--tier"}).text.strip().replace(" ", "")
+                Summoner_Wins = result.find(
+                    "div", attrs={"class": "winratio-graph__text winratio-graph__text--left"}).text.strip()
+                Summoner_Loses = result.find(
+                    "div", attrs={"class": "winratio-graph__text winratio-graph__text--right"}).text.strip()
+            except AttributeError:
+                continue
+            Summoner_Total_Games = int(Summoner_Wins) + int(Summoner_Loses)
+            # Reached bottom rank, skip summoner
+            check_rank = ''.join(
+                i for i in Current_Rank if not i.isnumeric()).lower()
+            if check_rank not in self.rank_array[:self.target_index]:
+                self.bad_rank_int += 1
+                pass
+            else:
+                summoner_dict[SummonerName] = [
+                    Current_Rank, str(Summoner_Total_Games)]
+        return summoner_dict
+
+    def write_to_file(self, _dict):
         """ Write summoner name and summoner rank from a dictionary to .txt file"""
 
         # Check if File Exists. If it doesn't, create one
-        if not os.path.exists(self.fileOut):
-            open(self.fileOut, 'w').close()
-        
+        if not os.path.exists(self.file):
+            open(self.file, 'w').close()
+
         # Write to file only if Summoner does not currently exists already
-        with open(self.fileOut, "r+", encoding="utf-8") as file:
-            while True:
+        with open(self.file, "r+", encoding="utf-8") as file:
 
-                input_ = ""
-                input_ = self.writerQueue.get()
-                
-                # If kill message is sent, break while loop
-                if input_ == "kill":
-                    break
-
-                summoner = input_.split("/")[0]
-                rank = input_.split("/")[1]
-                games = input_.split("/")[2]
-                full_input = summoner.ljust(16) + "\t\t" + rank + "\t\t" + str(games) + "\n"
-                
+            dict_to_write = _dict
+            for key in dict_to_write.keys():
+                summoner = key
+                rank = dict_to_write[key][0]
+                games = dict_to_write[key][1]
+                full_input = summoner.ljust(
+                    16) + "\t\t" + rank + "\t\t" + str(games) + "\n"
                 for line in file:
                     if summoner.ljust(16) in line and rank in line:
                         break
                 else:
                     file.write(full_input)
                     file.flush()
-
-    def parse_html(self, html):
-        """ Parse HTML for summoner name and rank and return a dictionary"""
-
-        summoner_dict = {}
-        bad_rank_count = 0
-        
-        results = html.find_all("tr", attrs={"class": "ranking-table__row"})
-        for result in results:
-            SummonerName = result.find(
-                  "td", attrs={"class": "select_summoner ranking-table__cell ranking-table__cell--summoner"}).select_one("span").text
-            Current_Rank = result.find(
-                   "td", attrs={"class": "ranking-table__cell ranking-table__cell--tier"}).text.strip().replace(" ", "")
-            Summoner_Wins = result.find(
-                "div", attrs={"class": "winratio-graph__text winratio-graph__text--left"}).text.strip()
-            Summoner_Loses = result.find(
-                "div", attrs={"class": "winratio-graph__text winratio-graph__text--right"}).text.strip()
-            Summoner_Total_Games = int(Summoner_Wins) + int(Summoner_Loses)
-            # Reached bottom rank, skip summoner
-            check_rank = ''.join(i for i in Current_Rank if not i.isnumeric())
-            if check_rank not in self.rank_array[:self.target_index]:
-                bad_rank_count += 1
-                pass
-            else:
-                summoner_dict[SummonerName] = [Current_Rank, str(Summoner_Total_Games)]
-        return summoner_dict, bad_rank_count
--- Main.py
+++ Main.py
@@ -8,24 +8,7 @@ class MainDriver(object):
 
         self.files = list()
         self.Regions_opgg = ["na", "kr", "euw", "oce", "eune", "lan"]
-
-    def run(self):
-        
-        baseRank = input("Enter lowest acceptable rank: ")
-        for region in self.Regions_opgg:
-            if region == "kr":
-                BaseURL = 'https://www.op.gg/ranking/ladder/'
-            else:
-                BaseURL = 'https://{}.op.gg/ranking/ladder/'.format(region)
-            
-            file = "{}_summoners.txt".format(region)
-            self.files.append(file)
-
-            # Instantiate a new GGScraper and run
-            print("Parsing Region: " + region)
-            Scraper = GGScrape.GGScraper(BaseURL, file, region, baseRank)
-            Scraper.runCrawler()
-
+        self.file = ""
 
     def sort_summoner_file(self):
 
@@ -33,18 +16,33 @@ class MainDriver(object):
         for file in self.files:
             print("[+] Sorting " + file)
             with open(file, 'r', encoding="utf-8") as fi, \
-                open(tmp_file, 'w', encoding="utf-8") as fo:
+                    open(tmp_file, 'w', encoding="utf-8") as fo:
                 lines = (line.rstrip() for line in fi)
-                unique_lines = OrderedDict.fromkeys( (line for line in lines if line ))
+                unique_lines = OrderedDict.fromkeys(
+                    (line for line in lines if line))
                 for key in unique_lines.keys():
                     fo.write(key + "\n")
             os.remove(file)
             os.rename(tmp_file, file)
 
 
-
 if __name__ == '__main__':
 
-    Scraper = MainDriver()
-    Scraper.run()
-    Scraper.sort_summoner_file()
+    Draftsmart = MainDriver()
+
+    baseRank = input("Enter lowest acceptable rank: ")
+    baseRegion = input("Enter region: ")
+    if baseRegion == "kr":
+        BaseURL = 'https://www.op.gg/ranking/ladder/'
+    else:
+        BaseURL = 'https://{}.op.gg/ranking/ladder/'.format(baseRegion)
+
+    Draftsmart.file = "{}_summoners.txt".format(baseRegion)
+
+    # Instantiate a new GGScraper and run
+    print("Parsing Region: " + baseRegion)
+    Scraper = GGScrape.GGScraper(
+        BaseURL, Draftsmart.file, baseRegion, baseRank)
+
+    # Clean up duplicates in file
+    Draftsmart.sort_summoner_file()
